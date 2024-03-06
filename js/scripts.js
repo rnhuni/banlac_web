124b37dc02a45a00c3065c51f5cb7bc957b597a9
Luis Daniel Angel <l.angelb@uniandes.edu.co>
2024-03-06 15:19:59 -0500

    Desarrollo pantallas banco de leche sumarizada y día

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,33 @@
-/*!
-* Start Bootstrap - Small Business v5.0.6 (https://startbootstrap.com/template/small-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+document.addEventListener("DOMContentLoaded", function() {
+  // Selecciona todas las pestañas
+  var tabs = document.querySelectorAll(".nav-tabs .nav-item .nav-link");
+
+  tabs.forEach(function(tab) {
+    tab.addEventListener("click", function(e) {
+        e.preventDefault(); // Prevenir el comportamiento predeterminado del enlace
+
+        // Remueve la clase 'active' de todas las pestañas
+        tabs.forEach(function(t) {
+        t.classList.remove("active");
+        });
+
+        // Agrega la clase 'active' a la pestaña clickeada
+        this.classList.add("active");
+
+        // Obtiene el valor del atributo 'data-target'
+        var targetId = this.getAttribute("data-target");
+
+        // Oculta todos los contenedores de contenido
+        var contents = document.querySelectorAll(".card-body > div");
+        contents.forEach(function(content) {
+            content.classList.add("d-none");
+        });
+
+        // Muestra el contenedor de contenido que coincide con el 'data-target'
+        var targetContent = document.querySelector(targetId);
+        if (targetContent) {
+            targetContent.classList.remove("d-none");
+        }
+    });
+  });
+});
